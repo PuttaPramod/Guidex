fa8fc992bba1c45b066fe314801686778c1e10f2
PuttaPramod <pramodkumarputta06@gmail.com>
2025-09-05 15:04:47 +0530

    final

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
+    path: 'roadmap/:link',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      // Define all the roadmap links you want to prerender
+      return [
+        { link: 'getting-started' },
+        { link: 'advanced-guide' },
+        { link: 'deployment' },
+        { link: 'troubleshooting' },
+        // Add more roadmap links as needed
+      ];
+    }
   }
 ];
