49e8e942f6eadba8e614d07655f39d3de5d36db3
PuttaPramod <pramodkumarputta06@gmail.com>
2025-09-25 17:38:13 +0530

    added scrolling to top

---
+++ .angular/cache/20.3.3/future-guidelines/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-OFL5BJBJ.js";
+import "./chunk-BCZQXLA7.js";
+import "./chunk-YTFLZFYN.js";
+import "./chunk-72SDWS7E.js";
+import "./chunk-SHQINJI2.js";
+import "./chunk-XZZHE5PL.js";
+import "./chunk-O5J3CNTX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
