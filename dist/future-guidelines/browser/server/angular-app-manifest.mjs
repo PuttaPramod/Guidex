c7f501faf491582ae0a3b75614ffcda356fa4bb0
PuttaPramod <pramodkumarputta06@gmail.com>
2025-10-21 15:07:31 +0530

    added chatbot2

--- dist/future-guidelines/browser/server/angular-app-manifest.mjs
+++ dist/future-guidelines/browser/server/angular-app-manifest.mjs
@@ -32,8 +32,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5396, hash: 'c9573ef8a1c21f9d58fe23a5231586a7279abbe473b76861e16279c9ea09d6fa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1038, hash: '37e35710088f9c2d976cda0e46a3a783b4d85a2dcfeea31fa98d9c95ed107430', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5455, hash: 'd96a9cc554f40f57e609180a26f6613d93a16f4d544607e34e22e832a316b7dc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1097, hash: '510ff5a5d134c55a60c4d43834bc3b1c760756ccd36e4ef3260392aae2d4f1ab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-IP3RSR46.css': {size: 391704, hash: 'WRqs80OnQFA', text: () => import('./assets-chunks/styles-IP3RSR46_css.mjs').then(m => m.default)}
   },
 };
