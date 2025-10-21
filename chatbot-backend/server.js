bb5e857217e46de6dde43b320d0583918c655f4b
PuttaPramod <pramodkumarputta06@gmail.com>
2025-10-21 15:24:24 +0530

    added chatbot

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: ["http://localhost:4200","pkfor10th.vercel.app"], // Your frontend URL
   credentials: true
 }));
 app.use(express.json());
